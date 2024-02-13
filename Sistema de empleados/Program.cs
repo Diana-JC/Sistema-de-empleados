cfacbf83ebd9791e3fcd6a7675ab4601a37b5618
dc552 <dc552@DESKTOP-80ATPO4>
2024-02-12 20:34:17 -0400

    Agregar archivos de proyecto.

---
+++ Sistema de empleados/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+
+class Empleado
+{
+   
+    public string? Nombre { get; set; }
+    public decimal Salario { get; set; }
+    
+
+    
+    public virtual void MostrarInformacion()
+    {
+        Console.WriteLine($"Nombre: {Nombre}");
+        Console.WriteLine($"Salario: {Salario}");
+        
+    }
+}
+
+
+class Gerente : Empleado
+{
+   
+    public string? Cargo { get; set; }
+
+    public override void MostrarInformacion()
+    {
+        base.MostrarInformacion();
+        Console.WriteLine($"Cargo: {Cargo}");
+    }
+}
+
+
+class Trabajador : Empleado
+{
+    
+    public string? Departamento { get; set; }
+
+   
+    public override void MostrarInformacion()
+    {
+        base.MostrarInformacion();
+        Console.WriteLine($"Tipo de trabajo: {Departamento}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+      
+
+        Gerente gerente = new Gerente
+        {
+            Nombre = "Ana Gómez",
+            Salario = 80000,
+            Cargo = "Gerente de Finanzas"
+        };
+
+        Trabajador trabajador = new Trabajador
+        {
+            Nombre = "Luis Rodríguez",
+            Salario = 35000,
+            Departamento = "Producción",
+           
+        };
+
+        
+        
+        Console.WriteLine("\nInformación del gerente:");
+        gerente.MostrarInformacion();
+
+        Console.WriteLine("\nInformación del trabajador:");
+        trabajador.MostrarInformacion();
+    }
+}
+
